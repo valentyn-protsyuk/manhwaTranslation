3da892d8e9772a304a06d304649729a75e15ca9a
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-23 21:24:12 -0800

    fix

--- experimental/readImg.py
+++ experimental/readImg.py
@@ -2,20 +2,22 @@ from PIL import Image
 from pytesseract import pytesseract
 
 img_kor = 'C:\myProjects\CTvsc\imgs\pietroch1kor.png'
-bad_img_en = 'C:\myProjects\comicTranslation\experimental\imgs\enBadImg.png'
-ok_img_en = 'C:\myProjects\comicTranslation\experimental\imgs\enSomeNoise.png'
+bad_img_eng = 'C:\myProjects\comicTranslation\experimental\imgs\enBadImg.png'
+ok_img_eng = 'C:\myProjects\comicTranslation\experimental\imgs\enSomeNoise.png'
 
-myconfig = r'--psm 3 --oem 3'
-# r'--psm 3 --oem 3'
-# r'-c tessedit_char_whitelist="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.!? " --psm 3'
+my_config = r'--psm 3 --oem 3'
 
 pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-korean = pytesseract.image_to_string(Image.open(img_kor), config=myconfig, lang='kor')
-bad_en = pytesseract.image_to_string(Image.open(bad_img_en), config=myconfig, lang='eng')
+korean = pytesseract.image_to_string(Image.open(img_kor), config=my_config, lang='kor')
+bad_eng = pytesseract.image_to_string(Image.open(bad_img_eng), config=my_config, lang='eng')
+ok_eng = pytesseract.image_to_string(Image.open(ok_img_eng), config=my_config, lang='eng')
 
 print('korean:')
 print(korean)
 
 print('Bad English:')
-print(bad_en)
+print(bad_eng)
+
+print('Ok English:')
+print(ok_eng)
