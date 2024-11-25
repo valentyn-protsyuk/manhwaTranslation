0710a5a576ddc4c1c742088591d3bc4b9277ff1d
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-24 21:14:46 -0800

    translation & alignment fixes

--- manhwaTranslate.py
+++ manhwaTranslate.py
@@ -39,8 +39,11 @@ def image_to_text():
         text = 'Insert text'
         canvas.create_text(100, 50, text=translated_text)
 
+
 def take_screenshot():
     global img
+    global text
+    text = None
     window.iconify()  # minimize window to take screenshot of the whole screen
     time.sleep(0.2)
     img = pyautogui.screenshot()  # take screenshot
@@ -50,17 +53,22 @@ def take_screenshot():
 def crop_screenshot(event=None):
     global cur_cropped_img
 
-    if img is not None:
+    if img is not None and text is None:
         dimensions = window.geometry()  # gets string with format: WxH+X+Y
         wh, x, y = dimensions.split('+')
         w, h = wh.split('x')
 
-        cropbox = (int(x),int(y), int(x) + int(w), int(y) + int(h)) 
+        tit_y = 82  # title bar offset
+        bor_x = 8  # border offset
+
+        cropbox = (int(x) + bor_x, int(y) + tit_y, int(x) + bor_x + int(w), int(y) + int(h))  # + tit_y
         cropped_img = img.crop(box=cropbox)
         cur_cropped_img = cropped_img
         cropped_img = ImageTk.PhotoImage(cropped_img)
+        canvas.delete("all")
         canvas.create_image(0, 0, anchor='nw', image=cropped_img)
         canvas.image = cropped_img
+
 
 select_area_btn = tkinter.Button(window, text='Select Img area', command=take_screenshot)
 select_area_btn.pack()
